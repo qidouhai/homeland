b3d92dec508fadd11827d92a9f67dc099cd397a0
lihuazhang@hotmail.com <lihuazhang@hotmail.com>
2017-02-22 13:09:49 +0800

    漏掉两个controller 没有更新

--- app/controllers/opencourses_controller.rb
+++ app/controllers/opencourses_controller.rb
@@ -12,7 +12,7 @@ class OpencoursesController < ApplicationController
     suggest_topic_ids = @suggest_topics.map(&:id)
     @topics = @node.topics.last_actived.fields_for_list
     @topics = @topics.where.not(id: suggest_topic_ids) if suggest_topic_ids.count > 0
-    @topics = @topics.includes(:user).paginate(page: params[:page], per_page: 30)
+    @topics = @topics.includes(:user).page(params[:page])
     @page_title = "#{t('menu.open_course')} - #{t('menu.topics')}"
     render '/topics/index' if stale?(etag: [@node, @suggest_topics, @topics], template: '/topics/index')
   end
