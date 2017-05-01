b58a4e3e1416c261c3be96c06f690ea6a3ee04b5
lihua zhang <lihuazhang@hotmail.com>
2017-05-01 09:51:23 +0800

    继续更新首页

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < ApplicationController
   def index
-    @excellent_topics = Topic.excellent.recent.fields_for_list.limit(30).to_a
+    @excellent_topics = Topic.excellent.recent.fields_for_list.limit(20).to_a
     @suggest_topics = Topic.without_hide_nodes.suggest.fields_for_list.limit(4).to_a
-    @latest_topics = Topic.recent.without_hide_nodes.with_replies_or_likes.fields_for_list.limit(30).to_a
+    @latest_topics = Topic.recent.without_hide_nodes.with_replies_or_likes.fields_for_list.limit(20).to_a
     @job_node = Node.find(Node.job_id)
     @job_topics = @job_node.topics.last_actived.without_hide_nodes.fields_for_list.limit(4).to_a
     @question_node = Node.find(Node.questions_id)
-    @question_topics = @question_node.topics.last_actived.without_hide_nodes.fields_for_list.limit(30).to_a
-    @users = User.normal.without_team.hot.limit(500).select { |user| user.topics.excellent.size >= 8 and user.level != "admin" }
+    @question_topics = @question_node.topics.last_actived.without_hide_nodes.fields_for_list.limit(20).to_a
+    @users = User.normal.without_team.hot.limit(500).select { |user| user.topics.excellent.size >= 8 and user.id != 110 }
   end
 
   def uploads
