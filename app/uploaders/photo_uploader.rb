4e10c5e418d6e774b53d24d8fd0063a20a0607a3
lihuazhang <lihuazhang@lihuazhangdeMacBook-Pro.local>
2018-02-04 20:08:01 +0800

    加个水印玩玩

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,4 +1,9 @@
 class PhotoUploader < BaseUploader
+  include CarrierWave::MiniMagick
+
+  process :add_text
+
+
   # Override the filename of the uploaded files:
   def filename
     if super.present?
@@ -6,4 +11,28 @@ class PhotoUploader < BaseUploader
       "#{Time.now.year}/#{@name}.#{file.extension.downcase}"
     end
   end
+
+  def watermark
+    watermark = "#{Rails.root}/app/assets/images/favicon.png"
+    manipulate! do |img|
+      img = img.composite(MiniMagick::Image.open(watermark), "png") do |c|
+        c.gravity "SouthWest"
+      end
+    end
+  end
+
+  def add_text
+    manipulate! do |image|
+      height = image.height
+      size = height * (0.002) * 20
+      image.combine_options do |c|
+        c.gravity 'southwest'
+        c.pointsize "#{size}"
+        c.draw "text 0,0 'testerhome.com'"
+        c.fill '#ccc'
+      end
+      image
+    end
+  end
+
 end
