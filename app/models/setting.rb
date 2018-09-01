0537114b1d6a5199986d48eb3f0fe724ed4774ef
lihuazhang <lihuazhang@lihuazhangdeMacBook-Pro.local>
2018-09-01 09:43:59 +0800

    和恒捷的消息分组代码合并

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -44,7 +44,8 @@ class Setting < RailsSettings::Base
     topic_create_limit_interval
     topic_create_hour_limit_count
     use_github_only
-    new_from_github_notices 
+    new_from_github_notices
+    notification_sidebar_advertise
   )
 
   class << self
