2484456f3c29aaccc886a6a0ed3b6b24ee63834e
chenhengjie <chenhengjie123@gmail.com>
2019-01-13 23:09:42 +0800

    feat: 完成管理员举报的列表、跟进功能

---
+++ app/controllers/admin/tip_offs_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Admin
+  class TipOffsController < Admin::ApplicationController
+    before_action :set_tip_off, only: %i[edit update]
+
+    def index
+      @tipOffs = TipOff.all
+    end
+
+    def edit
+      @tipOff = TipOff.find(params[:id])
+    end
+
+    def update
+      if @tipOff.update(params.require(:tip_off).permit(:follower_id))
+        redirect_to(admin_tip_offs_path, notice: "举报跟进成功")
+      end
+    end
+
+    private
+
+    def set_tip_off
+      @tipOff = TipOff.find(params[:id])
+    end
+
+  end
+end
