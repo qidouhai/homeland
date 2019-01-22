9a95fe5f9db4e86ff85d1567bbf0b446dcc134c6
chenhengjie <chenhengjie123@gmail.com>
2019-01-22 08:37:55 +0800

    docs: 举报创建表的脚本增加各个字段说明的注释

--- db/migrate/20181216122828_create_tip_offs.rb
+++ db/migrate/20181216122828_create_tip_offs.rb
@@ -1,15 +1,15 @@
 class CreateTipOffs < ActiveRecord::Migration[5.2]
   def change
     create_table :tip_offs do |t|
-      t.integer :reporter_id
-      t.string :reporter_email
-      t.string :tip_off_type
-      t.string :body
-      t.datetime :create_time
-      t.string :content_url
-      t.integer :follower_id
-      t.datetime :follow_time
-      t.datetime :deleted_at
+      t.integer :reporter_id # 举报人用户id
+      t.string :reporter_email # 举报人邮箱地址，用于回复
+      t.string :tip_off_type # 举报类型（垃圾广告、违规内容、不友善内容、其它理由）
+      t.string :body # 举报说明
+      t.datetime :create_time # 举报时间
+      t.string :content_url # 被举报内容 url
+      t.integer :follower_id # 跟进的管理员用户id
+      t.datetime :follow_time # 跟进时间
+      t.datetime :deleted_at # 删除时间
     end
   end
 end
