84242d0dce1607084af9006380bbe5bdb24ba426
chenhengjie <chenhengjie123@gmail.com>
2019-02-17 21:47:22 +0800

    refactor: 调整下索引，举报的索引只需要举报人 id 就可以满足目前需要了

--- db/migrate/20190217033059_add_index_to_tip_off.rb
+++ db/migrate/20190217033059_add_index_to_tip_off.rb
@@ -2,8 +2,5 @@ class AddIndexToTipOff < ActiveRecord::Migration[5.2]
   def change
     # "我的举报" 会根据举报人搜索
     add_index :tip_offs, :reporter_id
-
-    # 管理员批量处理举报时，会根据 content_url 搜索
-    add_index :tip_offs, :content_url
   end
 end
